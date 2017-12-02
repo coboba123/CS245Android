b874499b332d5f3fa347e52bb524d4e5da818cb1
Albert <agil6790@yahoo.com>
2017-12-01 19:00:09 -0800

    Animation from splash screen to home screen finished Changed some fonts working on getting buttons working

--- app/src/main/java/cpp/cs245final/MenuActivity.java
+++ app/src/main/java/cpp/cs245final/MenuActivity.java
@@ -1,13 +1,41 @@
 package cpp.cs245final;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        final Button startGameButton = findViewById(R.id.startGameButton);
+        startGameButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view){
+
+        switch (view.getId()) {
+            case R.id.startGameButton: // IF USER HITS NEW GAME BUTTON ON MAIN MENU SCREEN
+                Toast.makeText(getBaseContext(),"Starting Game",Toast.LENGTH_LONG).show();
+                final Intent startGame = new Intent(this,MainActivity.class);
+                startActivity(startGame);
+                break;
+
+            case R.id.newGameButton: // IF USER HITS HIGHSCORE BUTTON ON MAIN MENU SCREEN
+                Toast.makeText(getBaseContext(),"Displaying hiscores",Toast.LENGTH_LONG).show();
+                break;
+
+
+            default:
+                break;
+        }
+
     }
 }
