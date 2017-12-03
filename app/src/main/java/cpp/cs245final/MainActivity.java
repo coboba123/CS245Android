e93538f220a84d23646dc518bc27bb6a78522e02
Albert <agil6790@yahoo.com>
2017-12-02 20:03:48 -0800

    Finished most of layout Gota make files for landscape view.

--- app/src/main/java/cpp/cs245final/MainActivity.java
+++ app/src/main/java/cpp/cs245final/MainActivity.java
@@ -25,6 +25,8 @@ import android.widget.ToggleButton;
 
 import java.io.IOException;
 
+import static java.lang.Thread.sleep;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
     public Button tryAgainButton;
@@ -33,6 +35,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public Intent intent;
     public String difficultyLevel;
     private Button[] buttonArray;
+    public Button b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14,b15,b16,b17,b18,b19,b20;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -40,7 +43,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         musicPlayer();
 
-        //Initalizing menuButtons
+        //Initializing menuButtons
         tryAgainButton = findViewById(R.id.tryAgainButton);
         newGameButton = findViewById(R.id.newGameButton);
         endGameButton = findViewById(R.id.endGameButton);
@@ -55,34 +58,48 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         //ON GAME STARTUP MESSAGE
         Toast.makeText(getBaseContext(),"Creating " + difficultyLevel+"x"+difficultyLevel+ " grid",Toast.LENGTH_SHORT).show();
-        Button b1 = findViewById(R.id.button1);
-        Button b2 = findViewById(R.id.button2);
-        Button b3 = findViewById(R.id.button3);
-        Button b4 = findViewById(R.id.button4);
-        Button b5 = findViewById(R.id.button5);
-        Button b6 = findViewById(R.id.button6);
-        Button b7 = findViewById(R.id.button7);
-        Button b8 = findViewById(R.id.button8);
-        Button b9 = findViewById(R.id.button9);
-        Button b10 = findViewById(R.id.button10);
-        Button b11 = findViewById(R.id.button11);
-        Button b12 = findViewById(R.id.button12);
-        Button b13 = findViewById(R.id.button13);
-        Button b14 = findViewById(R.id.button14);
-        Button b15 = findViewById(R.id.button15);
-        Button b16 = findViewById(R.id.button16);
-        Button b17 = findViewById(R.id.button17);
-        Button b18 = findViewById(R.id.button18);
-        Button b19 = findViewById(R.id.button19);
-        Button b20 = findViewById(R.id.button20);
+
+        //Initializing GameButtons
+        b1 = findViewById(R.id.button1);
+        b2 = findViewById(R.id.button2);
+        b3 = findViewById(R.id.button3);
+        b4 = findViewById(R.id.button4);
+        b5 = findViewById(R.id.button5);
+        b6 = findViewById(R.id.button6);
+        b7 = findViewById(R.id.button7);
+        b8 = findViewById(R.id.button8);
+        b9 = findViewById(R.id.button9);
+        b10 = findViewById(R.id.button10);
+        b11 = findViewById(R.id.button11);
+        b12 = findViewById(R.id.button12);
+        b13 = findViewById(R.id.button13);
+        b14 = findViewById(R.id.button14);
+        b15 = findViewById(R.id.button15);
+        b16 = findViewById(R.id.button16);
+        b17 = findViewById(R.id.button17);
+        b18 = findViewById(R.id.button18);
+        b19 = findViewById(R.id.button19);
+        b20 = findViewById(R.id.button20);
         buttonArray = new Button[]{b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14,b15,b16,b17,b18,b19,b20};
 
-        for(int i = 0; i < 20; i ++)
-        {
+        //TESTING IF ALL BUTTONS SHOW UP
+        for(int i = 0; i < 20; i ++){
             buttonArray[i].setOnClickListener(this);
             buttonArray[i].setVisibility(View.VISIBLE);
             buttonArray[i].setBackgroundResource(R.drawable.buttonshape);
-            buttonArray[i].setTextColor(Color.parseColor("#4F4F4F"));
+            buttonArray[i].setTextColor(Color.parseColor("#00000000"));
+        }
+
+    }
+
+    public void startGame(){
+        int numOfTiles = Integer.parseInt(difficultyLevel);
+
+        for (int i = 0; i < numOfTiles; i++) {
+            buttonArray[i].setOnClickListener(this);
+            buttonArray[i].setVisibility(View.VISIBLE);
+            buttonArray[i].setBackgroundResource(R.drawable.buttonshape);
+            buttonArray[i].setTextColor(Color.parseColor("#00000000"));
         }
     }
 
@@ -112,14 +129,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (view.getId()) {
             case R.id.tryAgainButton:
                 Toast.makeText(getBaseContext(),"hi cody",Toast.LENGTH_LONG).show();
+                revertTiles();
                 break;
 
             case R.id.newGameButton:
                 Toast.makeText(getBaseContext(),"New Game!",Toast.LENGTH_LONG).show();
+                newGame();
                 break;
 
             case R.id.endGameButton:
                 Toast.makeText(getBaseContext(),"Game Terminated!",Toast.LENGTH_LONG).show();
+                endGame();
                 break;
 
             default:
@@ -128,7 +148,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
+    private void revertTiles() {
+    }
 
+    private void newGame() {
+    }
+
+    private void endGame() {
+    }
 
 
 }
