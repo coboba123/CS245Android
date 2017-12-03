501e0f9070a0fff7702d560e93252509dfeea5a1
Albert <agil6790@yahoo.com>
2017-12-02 23:01:48 -0800

    State is saved between orientation changes now.

--- app/src/main/java/cpp/cs245final/HighScoreActivity.java
+++ app/src/main/java/cpp/cs245final/HighScoreActivity.java
@@ -13,16 +13,17 @@ public class HighScoreActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_high_score);
+
         final Intent goToMainMenu = new Intent(this,MenuActivity.class);
         final Button mainMenuButton = findViewById(R.id.hiScoreBackButton);
 
         mainMenuButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getBaseContext(),"Returning to main menu.", Toast.LENGTH_LONG).show();
-                startActivity(goToMainMenu);
-        }
-        });
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(getBaseContext(),"Returning to main menu.", Toast.LENGTH_LONG).show();
+                    startActivity(goToMainMenu);
+            }
+            });
         }
 
 
